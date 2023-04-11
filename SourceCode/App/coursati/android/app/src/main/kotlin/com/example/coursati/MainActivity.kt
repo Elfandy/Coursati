95d2e21d9a26d0625fd8e089be6742ee9e2dcca3
XDslipcy <rayanelfandi9@gmail.com>
2023-04-11 13:05:04 +0200

    creating flutter app files

---
+++ SourceCode/App/coursati/android/app/src/main/kotlin/com/example/coursati/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coursati
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ SourceCode/App/coursati/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'Main Screens/Home_Page.dart';
+
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
