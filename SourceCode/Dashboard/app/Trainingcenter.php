4562d23ee9815d9f91229e18f69e2c691bf97885
hamy <54281066+hamy-00@users.noreply.github.com>
2023-05-25 08:08:10 +0200

    Update tc model

--- SourceCode/Dashboard/app/Trainingcenter.php
+++ SourceCode/Dashboard/app/Trainingcenter.php
@@ -21,21 +21,27 @@ class Trainingcenter extends Model
     {
 
     $active = '0';
+
+    
     $name = $request['name'];
     $logo = $request['logo'];
     $openTime = $request['openTime'];
     $closeTime = $request['closeTime'];
     $address = $request['address'];
     $discreption = $request['discreption'];
-    $id= $request['id'];
+    //$id= $request['id'];
     $facebook = $request['facebook'];
     $whatsapp = $request['whatsapp'];
     $website = $request['website'];
     $email = $request['email'];
     $phonenumber = $request['phonenumber'];
     $TC = $request['TC'];
+
+    //filled from the app
     $userID = $request['userID'];
 
+
+    //fillable
     $data=array('name'=>$name,"logo"=>$logo,"openTime"=>$openTime,"closeTime"=>$closeTime,"address"=>$address,"discreption"=>$discreption,"facebook"=>$facebook,"whatsapp"=>$whatsapp
     ,"website"=>$website,"email"=>$email,"phonenumber"=>$phonenumber,"TC"=>$TC,"userID"=>$userID,"active"=>$active);
 
@@ -64,21 +70,19 @@ class Trainingcenter extends Model
     $closeTime = $request['closeTime'];
     $address = $request['address'];
     $discreption = $request['discreption'];
-    $id= $request['id'];
+    //$id= $request['id'];
     $facebook = $request['facebook'];
     $whatsapp = $request['whatsapp'];
     $website = $request['website'];
     $email = $request['email'];
     $phonenumber = $request['phonenumber'];
     $TC = $request['TC'];
-    $userID = $request['userID'];
     
     $data2=array('name'=>$name,"logo"=>$logo,"openTime"=>$openTime,"closeTime"=>$closeTime,"address"=>$address,"discreption"=>$discreption,"facebook"=>$facebook,"whatsapp"=>$whatsapp
     ,"website"=>$website,"email"=>$email,"phonenumber"=>$phonenumber,"TC"=>$TC,"userID"=>$userID,"active"=>$active);
-   
-
-
+   //get tc id
     $id= $request['id'];
+    
     DB::table('trainingcenter')->where('id',$id)->update($data2);
 
     $ok ='updated';
@@ -90,23 +94,99 @@ class Trainingcenter extends Model
     //notificaiton
     public function noti(request $request)
     {
-
         $userID= $request['userID'];
-
         return  DB::table('trainingcenter')->where('userID',$userID)->value('notificaiton');
-       
-
     }
 
+
     //pending
-
-    public function pending(Request $request) {
-             
+    public function pending(Request $request) {   
         $userID= $request['userID'];
-
         return $this->orderBy('id', 'ASC')->where('active','0')->where('userID',$userID)->get();   
     }
 
+    public function rate(Request $request) {   
+
+        $rate= $request['rate'];
+        $id= $request['id'];
+
+        $allrate=DB::table('trainingcenter')->where('id',$id)->value('allrating');
+        $count=DB::table('trainingcenter')->where('id',$id)->value('ratingcount');
+
+        
+        $newratecount =$count + 1 ;
+        
+        $newRate=($allrate + $rate) / $newratecount;
+     
+        DB::table('trainingcenter')->where('id',$id)->update(array('rating'=>$newRate));
+        DB::table('trainingcenter')->where('id',$id)->update(array('ratingcount'=>$newratecount));
+        $newAllrating = $allrate+$rate;
+        DB::table('trainingcenter')->where('id',$id)->update(array('allrating'=>$newAllrating));
+
+        return 'done';
+    }
+
+    //search functionality 
+     public function search(Request $request) {   
+        $type= $request['type'];
+        $search= $request['search'];
+        $tags= $request['tags'];
+       $arraytags = array_map('intval', explode(',', $tags));
+      
+       // $tags = [1,2];
+        if($type=='training center')
+        {
+           if($tags==null){
+            return DB::table('trainingcenter')->orderBy('created_at', 'DESC')
+                                                ->where('name','like','%'.$search.'%')
+                                                ->select(array('id','name','image','tags'))->get();
+
+           }
+           else{
+            $x = collect();
+        foreach($arraytags as $tag){
+             $x = $x ->concat( DB::table('trainingcenter')->where('tags','like','%'.$tag.'%') 
+                                              ->where('name','like','%'.$search.'%')
+                                              ->select(array('id','name','image','tags'))
+                                              ->orderBy('created_at', 'DESC')->get());
+        } 
+        $x = $x -> unique();
+        return $x ->values()->all();
+    }      
+        }
+             else if($type=='course')
+        
+             if($tags==null){
+                return DB::table('course')->orderBy('created_at', 'DESC')
+                                                    ->where('name','like','%'.$search.'%')
+                                                    ->select(array('id','name','image','tags'))->get();
+    
+               }
+               else{
+                $x = collect();
+            foreach($arraytags as $tag){
+                 $x = $x ->concat( DB::table('course')->where('tags','like','%'.$tag.'%') 
+                                                  ->where('name','like','%'.$search.'%')
+                                                  ->select(array('id','name','image','tags'))
+                                                  ->orderBy('created_at', 'DESC')->get());
+            } 
+            $x = $x -> unique();
+            return $x ->values()->all();
+        }
+    
+        else {
+            $courselist = DB::table('Course')->where('name','like','%'.$search.'%')->select(array('id','name','image'))->get();
+            $Tclsit = DB::table('trainingcenter')->where('name','like','%'.$search.'%')->select(array('id','name','image'))->get();
+            return [
+                
+                'listCourse'=>$courselist,
+                'listTC'=>$Tclsit,
+
+
+        ];}
+         
+        
+    }
 
 
 
